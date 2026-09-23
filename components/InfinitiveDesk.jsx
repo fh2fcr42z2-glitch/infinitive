224361c73d60dba76867ba04535b192ae4640042
fh2fcr42z2-glitch <fh2fcr42z2@privaterelay.appleid.com>
2026-09-22 20:27:27 -0500

    InfinitiveDesk uses root style.css and markup module

--- components/InfinitiveDesk.jsx
+++ components/InfinitiveDesk.jsx
@@ -1,16 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
-import { CSS } from "./desk.css.js";
+import "../style.css";
 import { MARKUP } from "./deskMarkup.js";
 import { initDesk } from "./deskEngine.js";
 
 export default function InfinitiveDesk() {
   useEffect(() => initDesk(), []);
-  return (
-    <>
-      <style dangerouslySetInnerHTML={{ __html: CSS }} />
-      <div dangerouslySetInnerHTML={{ __html: MARKUP }} />
-    </>
-  );
+  return <div dangerouslySetInnerHTML={{ __html: MARKUP }} />;
 }
